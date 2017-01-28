3c680ee4ee8816c97442e49f4c351ad447fd21d2
Ali <cherifiali@outlook.fr>
2017-01-28 15:25:01 +0100

    Ajout des constructeurs pour l'imageView

--- app/src/main/java/com/soft/ali/traitementimage/ImgView.java
+++ app/src/main/java/com/soft/ali/traitementimage/ImgView.java
@@ -1,8 +1,32 @@
 package com.soft.ali.traitementimage;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
 /**
  * Created by ali on 27/01/2017.
  */
 
-public class ImgView {
+/**
+ * Making our own ImageView class. It allows us to control what's displayed on the screen.
+ * We will keep the basic display (Android rescaling)  and then it will be possible to zoom and scroll on the image.
+ */
+
+public class ImgView  extends ImageView {
+
+    public ImgView(Context context) {
+        super(context);
+    }
+
+    public ImgView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImgView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
