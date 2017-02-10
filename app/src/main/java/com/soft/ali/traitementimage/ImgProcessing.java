d35747813a57a9ce8a39c8e9bdc806bb9dda664a
Ali <cherifiali@outlook.fr>
2017-02-10 14:32:33 +0100

    Ajout de colorize

--- app/src/main/java/com/soft/ali/traitementimage/ImgProcessing.java
+++ app/src/main/java/com/soft/ali/traitementimage/ImgProcessing.java
@@ -1,8 +1,29 @@
 package com.soft.ali.traitementimage;
 
+import android.graphics.Color;
+
 /**
  * Created by ali on 27/01/2017.
  */
 
 public class ImgProcessing {
-}
+
+    Img image;
+
+    public void colorize(){
+        float hsv[] = new float[3];
+        int pixels[] = image.getArraypixel();
+        for (int i = 0; i < pixels.length; ++i) {
+
+            Color.colorToHSV(pixels[i], hsv);
+            hsv[0] = (float) 120; // TODO: Work with the color picker.
+            pixels[i] = Color.HSVToColor(hsv);
+        }
+
+
+        }
+    }
+
+
+
+
