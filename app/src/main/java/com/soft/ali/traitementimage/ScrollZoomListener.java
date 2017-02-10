4a4e7625361fe6f86b6fcbd269d06865ff3c0053
Ali <cherifiali@outlook.fr>
2017-02-10 10:52:17 +0100

    Correction mineure.

--- app/src/main/java/com/soft/ali/traitementimage/ScrollZoomListener.java
+++ app/src/main/java/com/soft/ali/traitementimage/ScrollZoomListener.java
@@ -3,6 +3,7 @@ package com.soft.ali.traitementimage;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by ali on 28/01/2017.
@@ -16,6 +17,9 @@ import android.view.View;
  */
 public class ScrollZoomListener implements View.OnTouchListener {
 
+    float curX = 0;
+    float curY = 0;
+
     /**
      * Overriding the onTouch method allows to handle zooming and scrolling on an image.
      * The method knows how many fingers are on the screen and so decides if the user wants to scroll or zoom.
@@ -28,17 +32,33 @@ public class ScrollZoomListener implements View.OnTouchListener {
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
+
         // index is the index of the first finger in the event queue.
         int index = motionEvent.getActionIndex();
         //Getting the IDs of the others fingers.
         int pointerID = motionEvent.getPointerId(index);
 
+        //Cooridnates of the first finger ont the screen
+        float newX, newY;
+        //Coordinates of the moving finger
+
         switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
             //One finger on the screen.
             case MotionEvent.ACTION_DOWN:
-                //TODO scrolling the image.
                 Log.i("PTR", "One finger");
+                curX = motionEvent.getX();
+                curY = motionEvent.getY();
+                Log.i("VAL DOWN", String.valueOf(curX) + " "+String.valueOf(curY));
                 break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i("PTR", "Move");
+                newX = motionEvent.getX();
+                newY = motionEvent.getY();
+                view.scrollBy((int)(curX-newX), (int)(curY - newY));
+                curX = newX;
+                curY = newY;
+                break;
+
              //Multiple fingers on screen.
             case MotionEvent.ACTION_POINTER_DOWN:
                 //TODO zooming on the image.
