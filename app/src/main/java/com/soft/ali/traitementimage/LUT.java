927ae97ff0a7559d7ee912e6f7e9f1032b15a689
Ali <cherifiali@outlook.fr>
2017-02-11 09:48:15 +0100

    Implémentation de la LUT qui effectue ses calculs sur HSV.

--- app/src/main/java/com/soft/ali/traitementimage/LUT.java
+++ app/src/main/java/com/soft/ali/traitementimage/LUT.java
@@ -1,8 +1,61 @@
 package com.soft.ali.traitementimage;
 
+import android.graphics.Color;
+
 /**
  * Created by ali on 10/02/2017.
  */
 
 public class LUT {
+
+    private static final int NBCOLORS = 256;
+    private static final int CONTRAST = 255;
+
+    private  int[] table;
+    private int min = 255;
+    private int max = 0;
+
+    public LUT(){
+
+        table = new int[NBCOLORS];
+
+    }
+
+    public int getValueAt(int index){
+     return table[index];
+    }
+
+    public void generate(Img image){
+
+        int[] pixels = image.getArraypixel();
+
+        dynamique(pixels);
+
+        float[] hsv = new float[3];
+
+        for (int i = 0; i < table.length; i++){
+            Color.colorToHSV(pixels[i], hsv);
+            table[i] = getHistorgramEqualizationValue((int)hsv[1]);
+        }
+    }
+
+    public int getHistorgramEqualizationValue(int value){
+        return ( CONTRAST * (value - min) ) / (max - min);
+    }
+
+    private void dynamique(int[] pixels){
+
+        float[] hsv = new float[3];
+
+        for (int i = 0; i< pixels.length; i++){
+            Color.colorToHSV(pixels[i], hsv);
+            min = Math.min(min, (int)hsv[1]);
+            max = Math.max(max, (int)hsv[1]);
+        }
+    }
+
+
+
+
+
 }
