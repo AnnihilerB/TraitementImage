6e4dd5c3cc98843574b22f2a5c26a7d719dbc685
Ali <cherifiali@outlook.fr>
2017-02-13 18:35:51 +0100

    Implémentation de l'égalisation d'histogramme

--- app/src/main/java/com/soft/ali/traitementimage/ImgProcessing.java
+++ app/src/main/java/com/soft/ali/traitementimage/ImgProcessing.java
@@ -8,9 +8,9 @@ import android.graphics.Color;
 
 public class ImgProcessing {
 
-    Img image;
+    private static Img image;
 
-    public void colorize(){
+    public static void colorize() {
         float hsv[] = new float[3];
         int pixels[] = image.getArraypixel();
         for (int i = 0; i < pixels.length; ++i) {
@@ -19,16 +19,45 @@ public class ImgProcessing {
             hsv[0] = (float) 120; // TODO: Work with the color picker.
             pixels[i] = Color.HSVToColor(hsv);
         }
+    }
 
+    public static void histogramEqualization(){
 
+        Histogram hist = new Histogram();
+        hist.generateHisotgram(image);
+
+        float nbPixels = hist.nbpixels;
+        int pixels[] = image.getArraypixel();
+        float[] hsv = new float[3];
+
+        for (int i = 0; i < pixels.length; i++){
+            Color.colorToHSV(pixels[i], hsv);
+            hsv[Constants.HSV_SATURATION] = hist.getCumulativeHistogramValueAt((int)hsv[Constants.HSV_SATURATION]) * 255 / nbPixels;
+            pixels[i] = Color.HSVToColor(hsv);
         }
+    }
 
 
-    public void extensionDynamique(){
+    public void extensionDynamique() {
         //La faire en HSV et ne pas toucher la teinte. Faire l'extension sur S puis repasser en RGB.
     }
 
+    public void convolution() {
+        /* Filtre de taille impaire toujours.
+        Pose le filtre sur l'aimge, le filtre calcule la valeur du pixel central en mulitipliant la valeur des pixels par la valeur du masque 1 à1.
+        premiere case pixel * premiere case masque etc..
+         */
     }
+
+    public static void setImage(Img imagebase){
+        image = imagebase;
+    }
+
+    public static Img getImage(){
+        return image;
+    }
+
+}
 
 
 
