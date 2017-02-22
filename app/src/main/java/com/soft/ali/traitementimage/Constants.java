97cd4923c40815e6f7337b89ddab2750fadd4f2d
Ali <ali.cherifi@etu.u-bordeaux.fr>
2017-02-22 16:46:56 +0100

    Ajout de nouvelles constantes de fichiers et de camera

--- app/src/main/java/com/soft/ali/traitementimage/Constants.java
+++ app/src/main/java/com/soft/ali/traitementimage/Constants.java
@@ -6,6 +6,9 @@ package com.soft.ali.traitementimage;
 
 public class Constants {
 
+    //Activity constants
+    public static final int REQUEST_CAPTURE = 2;
+
     //Int constants
     public static final int NBCOLORS = 256;
 
@@ -16,6 +19,14 @@ public class Constants {
     public  static final int HSV_HUE = 0;
     public  static final int HSV_SATURATION = 1;
     public  static final int HSV_VIBRANCE = 2;
+
+    //Error constants
+    public static final String ERROR_WRITING_IMAGE = "ERRWR";
+    public final String ERROR_GETTING_IMAGE = "ERRGET";
+
+    //Camera constants
+    public static final String FILE_NAME = "imgTmp";
+    public static final String EXTENSION = ".png";
 
 
 
