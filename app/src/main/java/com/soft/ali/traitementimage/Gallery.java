115b48e99e28903208f43a933bb98e369342714b
Valentin Gaillard <a>
2017-02-28 00:53:04 +0100

    ajout gallery + filtre + traitements

--- app/src/main/java/com/soft/ali/traitementimage/Gallery.java
+++ app/src/main/java/com/soft/ali/traitementimage/Gallery.java
@@ -1,8 +1,70 @@
 package com.soft.ali.traitementimage;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+
 /**
  * Created by ali on 27/01/2017.
  */
 
-public class Gallery {
+public class Gallery extends AppCompatActivity {
+
+    // Image loading result to pass to startActivityForResult method.
+    private static int LOAD_IMAGE_RESULTS = 1;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startGallery();
+        ByteArrayOutputStream bs = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 50, bs);
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("byteArray", bs.toByteArray());
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Here we need to check if the activity that was triggers was the Image Gallery.
+        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
+        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
+        if (requestCode == LOAD_IMAGE_RESULTS && resultCode == RESULT_OK && data != null) {
+            // Let's read picked image data - its URI
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+
+            // Now we need to set the GUI ImageView data with data read from the picked file.
+            bitmap = BitmapFactory.decodeFile(imagePath);
+
+            // At the end remember to close the cursor or you will end with the RuntimeException!
+            cursor.close();
+
+        }
+    }
+
+    public void startGallery (){
+        // Create the Intent for Image Gallery.
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // Start new activity with the LOAD_IMAGE_RESULTS to handle back the results when image is picked from the Image Gallery.
+        startActivityForResult(i, LOAD_IMAGE_RESULTS);
+    }
 }
--- app/src/main/java/com/soft/ali/traitementimage/MainActivity.java
+++ app/src/main/java/com/soft/ali/traitementimage/MainActivity.java
@@ -1,13 +1,133 @@
 package com.soft.ali.traitementimage;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button buttonGallery;
+    private Button buttonProcessing;
+    private ImageView image;
+    private Img imagea;
+    private ImgProcessing imgProcessing;
+    private Bitmap bitmap ;
+    private int chosenColor;
+    private Bitmap bitmaptext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGallery = (Button)findViewById(R.id.button);
+        buttonProcessing = (Button)findViewById(R.id.button2);
+        image = (ImageView)findViewById(R.id.imageView);
+        buttonGallery.setOnClickListener(this);
+        buttonProcessing.setOnClickListener(this);
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        imagea = new Img(bitmap);
+        imgProcessing = new ImgProcessing(imagea);
+        bitmaptext = BitmapFactory.decodeResource(getResources(), R.mipmap.texte);
+
+        LinearGradient test = new LinearGradient(0.f, 0.f, 600.f, 0.0f, new int[] { 0xFF000000, 0xFF0000FF, 0xFF00FF00, 0xFF00FFFF, 0xFFFF0000, 0xFFFF00FF, 0xFFFFFF00, 0xFFFFFFFF}, null, Shader.TileMode.CLAMP);
+        ShapeDrawable shape = new ShapeDrawable(new RectShape());
+        shape.getPaint().setShader(test);
+
+        final SeekBar seekBarFont = (SeekBar)findViewById(R.id.seekbar_font);
+        seekBarFont.setProgressDrawable(shape);
+        seekBarFont.setMax(256*7-1);
+
+        seekBarFont.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    int r = 0;
+                    int g = 0;
+                    int b = 0;
+
+                    if(progress < 256){
+                        b = progress;
+                    } else if(progress < 256*2) {
+                        g = progress%256;
+                        b = 256 - progress%256;
+                    } else if(progress < 256*3) {
+                        g = 255;
+                        b = progress%256;
+                    } else if(progress < 256*4) {
+                        r = progress%256;
+                        g = 256 - progress%256;
+                        b = 256 - progress%256;
+                    } else if(progress < 256*5) {
+                        r = 255;
+                        g = 0;
+                        b = progress%256;
+                    } else if(progress < 256*6) {
+                        r = 255;
+                        g = progress%256;
+                        b = 256 - progress%256;
+                    } else if(progress < 256*7) {
+                        r = 255;
+                        g = 255;
+                        b = progress%256;
+                    }
+                    seekBarFont.setBackgroundColor(Color.argb(255, r, g, b));
+                    chosenColor=Color.argb(255, r, g, b);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.button) {
+            Intent intent = new Intent(MainActivity.this, Gallery.class);
+            startActivityForResult(intent, 1);
+        }
+
+        if(v.getId() == R.id.button2) {
+            imgProcessing.Colorize(chosenColor);
+            imgProcessing.fusion(bitmaptext);
+            imgProcessing.Overexposure();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case (1) : {
+                if (resultCode == Activity.RESULT_OK) {
+                    bitmap = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("byteArray"),0,getIntent().getByteArrayExtra("byteArray").length);
+                    image.setImageBitmap(bitmap);
+                }
+                break;
+            }
+        }
+    }
+
 }
