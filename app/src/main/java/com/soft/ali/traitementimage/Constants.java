ca76cd33021ab0e0c945211d725e1f05b185d71e
Ali <cherifiali@outlook.fr>
2017-02-28 14:47:00 +0100

    Ajout des constantes pour la convolution

--- app/src/main/java/com/soft/ali/traitementimage/Constants.java
+++ app/src/main/java/com/soft/ali/traitementimage/Constants.java
@@ -6,7 +6,15 @@ package com.soft.ali.traitementimage;
 
 public class Constants {
 
+    //Convolution constants
+    public static final int AVERAGE = 0;
+    public static final int GAUSS = 1;
+    public static final int SOBEL = 2;
+    public static final int LAPLACE = 3;
+    public static final int LAPLACE2 = 4;
+
     //Activity constants
+    public static int LOAD_IMAGE = 1;
     public static final int REQUEST_CAPTURE = 2;
     public static  final int WRITE_IMAGE = 3;
 
