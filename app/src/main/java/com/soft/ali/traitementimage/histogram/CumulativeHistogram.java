8e0f131d7b7049fdfed2b37fae1245fbba768cce
Ali <cherifiali@outlook.fr>
2017-03-28 22:53:43 +0200

    Ajout de la classe gérant un histogramme cumulé

---
+++ app/src/main/java/com/soft/ali/traitementimage/histogram/CumulativeHistogram.java
@@ -0,0 +1,33 @@
+package com.soft.ali.traitementimage.histogram;
+
+import com.soft.ali.traitementimage.Constants;
+
+/**
+ * Created by ali on 28/03/2017.
+ */
+
+public class CumulativeHistogram {
+
+    private int cumulHist[];
+
+    public CumulativeHistogram(){
+        cumulHist = new int[Constants.NBCOLORS];
+    }
+
+    public void generateCumulativeHistogram(int hist[]){
+        int sum = 0;
+        for (int i = 0; i < Constants.NBCOLORS; i++){
+            sum = hist[i] + sum;
+            cumulHist[i] = sum;
+        }
+    }
+
+    /**
+     * Getting the value in the cumulative histogram.
+     * @param index index of the array
+     * @return the value gathered at index.
+     */
+    public int getCumulativeHistogramValueAt(int index){
+        return cumulHist[index];
+    }
+}
